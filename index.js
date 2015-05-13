719d18cc42406eb71d29c1c1b68d6eb75527baba
Anshul Goel <anshul.goel@priceline.com>
2015-05-13 09:39:23 -0400

    Updated logic to delete files

--- index.js
+++ index.js
@@ -8,27 +8,41 @@ var pluginName = 'gulp-contrib-clean';
 
 module.exports = function( opts ) {
 
+  var deleteFolderFilesRecursive = function(path) {
+    if(fs.lstatSync(path).isDirectory()) {
+      if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file){
+          var currentPath = path + "/" + file;
+          if(fs.lstatSync(currentPath).isDirectory()) {
+            deleteFolderFilesRecursive(currentPath);
+          } else {
+            fs.unlinkSync(currentPath);
+          }
+        });
+        fs.rmdirSync(path);
+      }
+    } else if( fs.existsSync(path) ) {
+      fs.unlinkSync(path);
+    }
+  };
+
   function clean(file, encoding, callback) {
     var options = opts || {};
-
-    if (file.isNull()) {
-      return callback(null, file);
-    }
 
     if (file.isStream()) {
       return callback(new PluginError(pluginName, 'Streaming not supported'));
     }
 
-    if(file.isBuffer()) {
-      fs.unlinkSync(file.path);
+    if (file.isNull() || file.isDirectory()) {
+      deleteFolderFilesRecursive(file.path);
 
       if(options) {
         // TODO: Implement options
-        console.log('Options to be implemented')
+        console.log('Options to be implemented');
       }
-
-      callback( null, file );
     }
+
+    callback( null, file );
   }
 
   return through.obj(clean);
