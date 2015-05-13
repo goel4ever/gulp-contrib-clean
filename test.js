d190b26f4fe5b65f51d5313cc7888d3345f4ad9a
Anshul Goel <anshul.goel@priceline.com>
2015-05-13 11:06:56 -0400

    Updated the test file

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('Yo!!! Test is to be written');
+var index = require('./index.js'),
+    gulp  = require('gulp');
+
+gulp.task('test', function() {
+  gulp.src('node_modules/jquery')
+    .pipe(clean());
+});
+
+console.log('Yo!!! Test is to be written and somehow call the clean method');
